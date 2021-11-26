3c5dc10b1cd93eb3df3cbfce87037894939ce000
Gianluca Benassi <benox1199@gmail.com>
2021-11-26 14:19:07 +0100

    images array

--- js/script.js
+++ js/script.js
@@ -3,6 +3,34 @@ const app = new Vue({
 
     data: {
 
+        images = [
+            {
+            'title': 'Svezia',
+            'desc':  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            'img': 'img/01.jpg'
+            },
+            {
+            'title': 'Svizzera',
+            'desc':  'Lorem ipsum',
+            'img': 'img/02.jpg'
+            },
+            {
+            'title': 'Gran Bretagnia',
+            'desc':  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'img': 'img/03.jpg'
+            },
+            {
+            'title': 'Germania',
+            'desc':  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            'img': 'img/04.jpg'
+            },
+            {
+            'title': 'Paradise',
+            'desc':  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            'img': 'img/05.jpg'
+            }
+        ]
+        
     },
 
     methods: {
